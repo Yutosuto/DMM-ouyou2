fdd61d3ef4d1067d6af415af691cf82a0c6cbbe8
Yutosuto <suto.yuto@gmail.com>
2021-03-03 13:05:30 +0000

    [finish]ouyou2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,32 @@
 class BooksController < ApplicationController
-
+before_action :authenticate_user!,except:[:top, :about]
+before_action :ensure_correct_user,only: [:edit, :update, :destroy]
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      flash[:success] = 'You have updated user successfully.'
+      redirect_to book_path(@book)
     else
       @books = Book.all
+      @user = current_user
       render 'index'
     end
   end
 
   def edit
+    @user = Book.find(params[:id])
     @book = Book.find(params[:id])
   end
 
@@ -34,16 +41,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :users
+  resources :users,only: [:show,:index,:edit,:update]
   end 
